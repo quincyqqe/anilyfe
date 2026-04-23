68ab0fd3cbca79833b8ae2526404d2dbd190f86e
sxcre <sxcred06@gmail.com>
2026-04-24 00:31:40 +0300

    add random franchises

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,16 +1,29 @@
-import { fetchPromotions } from '@/features/home/api/promotions';
+import { fetchRandomFranchises } from '@/features/franchise/api/franchises';
 import { fetchLatest } from '@/features/home/api/latest';
+import { fetchPromotions } from '@/features/home/api/promotions';
 
-import { HomeFeatured as Featured, HomeLatest as Latest, HomeWatching as Watching } from '@/features/home';
+import {
+  HomeFeatured as Featured,
+  HomeLatest as Latest,
+  HomeWatching as Watching,
+  RandomFranchises,
+} from '@/features/home';
 
 const Page = async () => {
-  const [promotions, latestAnime] = await Promise.all([fetchPromotions(), fetchLatest()]);
+  const [promotions, latestAnime, randomFranchises] = await Promise.all([
+    fetchPromotions(),
+    fetchLatest(),
+    fetchRandomFranchises(),
+  ]);
+
+
 
   return (
     <main>
       <Featured promotions={promotions} />
       <Watching />
       {latestAnime.length > 0 && <Latest animeList={latestAnime} />}
+      {randomFranchises.length > 0 && <RandomFranchises animeList={randomFranchises} />}
     </main>
   );
 };
