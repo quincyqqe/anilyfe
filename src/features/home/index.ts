ce46172be1bafdacde3cbb69f1572e2ccd348902
sxcre <sxcred06@gmail.com>
2026-04-24 01:07:24 +0300

    update import

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeFeatured } from './featured';
 export { default as HomeLatest } from './latest';
 export { default as HomeWatching } from './watching';
+export { default as RandomFranchises } from './franchises';
