23349719b135f3654efc75ad84bcbfafbac22894
sxcre <sxcred06@gmail.com>
2026-08-17 17:37:00 +0300

    refactor: remove HeroUI

--- src/providers/ui-provider.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { HeroUIProvider } from '@heroui/react';
-
-export function UIProvider({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
-}
